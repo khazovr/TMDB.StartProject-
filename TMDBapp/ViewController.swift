0cbe1660286d1019bc9135c10b66c74a9c2e2dc2
khazovr <r44khazovr@gmail.com>
2022-01-10 16:35:03 +0200

    initial commit

---
+++ TMDBapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDBapp
+//
+//  Created by 1 on 10.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
