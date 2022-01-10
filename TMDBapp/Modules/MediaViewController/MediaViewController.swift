15e8baa58cf6264f7122fb7028a23ee6ecec3f34
khazovr <r44khazovr@gmail.com>
2022-01-10 17:39:11 +0200

    Add Podfile Add TableView Add MediaViewController folders

--- TMDBapp/Modules/MediaViewController/MediaViewController.swift
+++ TMDBapp/Modules/MediaViewController/MediaViewController.swift
@@ -9,9 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var listOfFilmsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
